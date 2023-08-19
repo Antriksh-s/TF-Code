53b947c35a17d15c26fc190eba2f834635375e07
sg-ankumar <ankumar@sgintl.com>
2023-08-19 11:16:22 +0530

    creating EC2

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
-#Terraform main.tf code goes here
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.13.1"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = ""
+    secret_key = ""
+}
+
+resource "aws_ec2_host" "test" {
+  ami               = "ami-08a52ddb321b32a8c"  
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1"
+  name              = "test-instance"
+}
+
